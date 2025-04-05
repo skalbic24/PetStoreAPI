3e6e14056bbad96c95996dba8d8f7f915dc80ded
Sanel Kalbic <sanelkalbic@Sanels-MacBook-Pro.local>
2025-04-05 15:07:57 -0500

    main

---
+++ src/main/java/pet/store/PetStoreApplication.java
@@ -0,0 +1,12 @@
+package pet.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Main application class to start Spring Boot
+@SpringBootApplication
+public class PetStoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PetStoreApplication.class, args);
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO amenity (amenity) VALUES ('');
+INSERT INTO amenity (amenity) VALUES ('');
+INSERT INTO amenity (amenity) VALUES ('');
+INSERT INTO amenity (amenity) VALUES ('');
+INSERT INTO amenity (amenity) VALUES ('');
+INSERT INTO amenity (amenity) VALUES ('');
+INSERT INTO amenity (amenity) VALUES ('');
+INSERT INTO amenity (amenity) VALUES ('');
+INSERT INTO amenity (amenity) VALUES ('');
+INSERT INTO amenity (amenity) VALUES ('');
+
